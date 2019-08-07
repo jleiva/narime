df510548d79655347aefd3fab5ee69bd787107bc
Jose Leiva <jdleiva@backcountry.com>
2019-08-07 17:54:02 -0600

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './src/css',
+        to: 'css' ,
+        force: true
+      },
+      {
+        from: './src/img',
+        to: 'img' ,
+        force: true
+      }
+    ])
+  ],
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript 
+  // minifying and other thing so let's set mode to development
+  mode: 'development'
+};
